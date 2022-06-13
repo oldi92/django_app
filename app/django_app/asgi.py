d48a729af57e5c329fe174a3a9c2b6f330ff0308
Oldi Prillo <oldi.prillo@gmail.com>
2022-06-13 10:51:38 +0100

    Dockerize django app for development and production

--- app/django_app/asgi.py
+++ app/django_app/asgi.py
@@ -9,16 +9,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
     }
 )
