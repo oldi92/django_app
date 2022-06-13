69a2cdd98513b37d93363f38c577428ca045c911
Oldi Prillo <oldi.prillo@gmail.com>
2022-06-13 17:44:21 +0100

    Refactor asgi application for production

--- app/django_app/asgi.py
+++ app/django_app/asgi.py
@@ -1,22 +1,18 @@
-"""
-ASGI config for django_app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
+        # WebSocket chat handler
         "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
     }
 )
